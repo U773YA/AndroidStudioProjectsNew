244853c9c1106a85881ac4fb8885af274cd7dc7d
U773YA <utteya.mcs17.du@gmail.com>
2018-08-19 12:30:31 +0530

    Completed Chapter 11 Challenge

---
+++ CriminalIntent Challenge Chapter 11/app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,92 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.List;
+import java.util.UUID;
+
+public class CrimePagerActivity extends AppCompatActivity {
+
+    private static final String EXTRA_CRIME_ID="com.bignerdranch.android.criminalintent.crime_id";
+
+    private ViewPager mViewPager;
+    private List<Crime> mCrimes;
+    private Button mFirst, mLast;
+
+    public static Intent newIntent(Context packageContext, UUID crimeId){
+        Intent intent=new Intent(packageContext,CrimePagerActivity.class);
+        intent.putExtra(EXTRA_CRIME_ID,crimeId);
+        return intent;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime_pager);
+
+        UUID crimeId=(UUID)getIntent().getSerializableExtra(EXTRA_CRIME_ID);
+
+        mViewPager=(ViewPager)findViewById(R.id.crime_view_pager);
+        mFirst=(Button)findViewById(R.id.first_button);
+        mLast=(Button)findViewById(R.id.last_button);
+
+        mCrimes=CrimeLab.get(this).getCrimes();
+        FragmentManager fragmentManager=getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fragmentManager) {
+            @Override
+            public Fragment getItem(int position) {
+                Crime crime=mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getId());
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
+
+        for(int i=0;i<mCrimes.size();i++){
+            if(mCrimes.get(i).getId().equals(crimeId)){
+                mViewPager.setCurrentItem(i);
+                break;
+            }
+        }
+
+        mViewPager.addOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                mFirst.setEnabled(position!=0);
+                mLast.setEnabled(position!=(mCrimes.size()-1));
+            }
+        });
+
+        if(mViewPager.getCurrentItem()==0)
+            mFirst.setEnabled(false);
+        mFirst.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(0);
+            }
+        });
+
+        if(mViewPager.getCurrentItem()==mCrimes.size()-1)
+            mLast.setEnabled(false);
+        mLast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mViewPager.setCurrentItem(mCrimes.size()-1);
+            }
+        });
+    }
+}
